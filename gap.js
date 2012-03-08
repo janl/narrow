f2431b88b5bbff402a209dc3d26efe3d74bb01a7
Jan Lehnardt <jan@apache.org>
2012-03-08 23:53:00 +0100

    flatten folder hierarchy

--- gap.js
+++ gap.js
@@ -1,14 +1,25 @@
 var gaps = [
   {
     phrasing: "",
-    occupation: ",edoca; scientists",
+    occupation: "Ärztinnen",
     earnings_gap: 78,
     less_per_week: 275,
     source: "ftp://ftp.bls.gov/pub/special.requests/lf/aat39.txt"
+  },
+  {
+    phrasing: "",
+    occupation: "Werbefachfrauen",
+    earnings_gap: 93,
+    less_per_week: 59,
+    source: "ftp://ftp.bls.gov/pub/special.requests/lf/aat39.txt"
   }
+
 ];
 
 var exclamations = [
-  "Woha",
-  "Boulderdash"
+  "Lachhaft!",
+  "Unsinn!",
+  "Wahnsinn!",
+  "Unglaublich!",
+  "Was?!"
 ];
