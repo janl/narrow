5e9e63f97b3c5e576c098602d9da46310701e86f
Jan Lehnardt <jan@apache.org>
2012-03-08 22:59:11 +0100

    init

---
+++ gap.js
@@ -0,0 +1,14 @@
+var gaps = [
+  {
+    phrasing: "",
+    occupation: ",edoca; scientists",
+    earnings_gap: 78,
+    less_per_week: 275,
+    source: "ftp://ftp.bls.gov/pub/special.requests/lf/aat39.txt"
+  }
+];
+
+var exclamations = [
+  "Woha",
+  "Boulderdash"
+];
