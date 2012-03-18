20f66f3a916eccfffaf10a6c8f8c31a7fecc6517
Anne Wizorek <anne.wizorek@gmail.com>
2012-03-18 16:22:17 +0100

    Eingepflegte Berufe und Zahlen: Bank- und Finanzberufe, Bauberufe.

--- gap.js
+++ gap.js
@@ -39,6 +39,150 @@ var gaps = [
     her: 2108,
     group_size: 1702
   }
+  {
+    phrasing: "",
+    occupation: "Buchprüferinnen",
+    group: "",
+    him: 3004,
+    her: 2788,
+    group_size: 335
+  },
+  {
+    phrasing: "",
+    occupation: "Finanzmaklerinnen",
+    group: "",
+    him: 3941,
+    her: 3410,
+    group_size: 93
+  },
+  {
+    phrasing: "",
+    occupation: "Kreditsachbearbeiterinnen",
+    group: "",
+    him: 2805,
+    her: 2526,
+    group_size: 327
+  },
+  {
+    phrasing: "",
+    occupation: "Steuerberaterinnen",
+    group: "",
+    him: 3164,
+    her: 2981,
+    group_size: 241
+  },
+  {
+    phrasing: "",
+    occupation: "Steuerfachangestellte",
+    group: "",
+    him: 2080,
+    her: 1993,
+    group_size: 773
+  },
+  {
+    phrasing: "",
+    occupation: "Steuerfachgehilfinnen",
+    group: "",
+    him: 2428,
+    her: 1751,
+    group_size: 108
+  },
+  {
+    phrasing: "",
+    occupation: "Steuerfachwirtinnen",
+    group: "",
+    him: 2480,
+    her: 2466,
+    group_size: 191
+  },
+  {
+    phrasing: "",
+    occupation: "Vermögensberaterinnen",
+    group: "",
+    him: 3531,
+    her: 3162,
+    group_size: 199
+  },
+  {
+    phrasing: "",
+    occupation: "Versicherungskauffrauen",
+    group: "",
+    him: 2300,
+    her: 2033,
+    group_size: 520
+  },
+  {
+    phrasing: "",
+    occupation: "Versicherungsvertreterinnen",
+    group: "",
+    him: 2512,
+    her: 2117,
+    group_size: 136
+  },
+  {
+    phrasing: "",
+    occupation: "Bauingenieurinnen",
+    group: "",
+    him: 3046,
+    her: 2699,
+    group_size: 1264
+  },
+  {
+    phrasing: "",
+    occupation: "Bauleiterinnen",
+    group: "",
+    him: 2898,
+    her: 2495,
+    group_size: 524
+  },
+  {
+    phrasing: "",
+    occupation: "Bautechnikerinnen",
+    group: "",
+    him: 2528,
+    her: 2206,
+    group_size: 424
+  },
+  {
+    phrasing: "",
+    occupation: "Landschaftsgärtnerinnen",
+    group: "",
+    him: 2012,
+    her: 1884,
+    group_size: 189
+  },
+  {
+    phrasing: "",
+    occupation: "Finanzmaklerinnen",
+    group: "",
+    him: 3941,
+    her: 3410,
+    group_size: 93
+  },
+  {
+    phrasing: "",
+    occupation: "Finanzmaklerinnen",
+    group: "",
+    him: 3941,
+    her: 3410,
+    group_size: 93
+  },
+  {
+    phrasing: "",
+    occupation: "Finanzmaklerinnen",
+    group: "",
+    him: 3941,
+    her: 3410,
+    group_size: 93
+  },
+  {
+    phrasing: "",
+    occupation: "Finanzmaklerinnen",
+    group: "",
+    him: 3941,
+    her: 3410,
+    group_size: 93
+  },
 ];
 
 var exclamations = [
